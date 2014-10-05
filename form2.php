c33e593354b07fd0bb38b655b135a06f0800e5bf
Chris Vergaray <ver08007@byui.edu>
2014-10-04 23:54:26 -0600

    Now perhaps the function will actually run as a PHP function

--- form2.php
+++ form2.php
@@ -23,8 +23,8 @@ fwrite($myfile, $_SESSION["website"]);
 fwrite($myfile, $_SESSION["comment"]);
 fwrite($myfile, $_SESSION["gender"]);
 fclose($myfile);
-//header("results2.php");
-//exit();
+header("location: http://php-cvergara.rhcloud.com/results2.php");
+exit();
 }
 ?>
 
