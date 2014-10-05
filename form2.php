ddd2dc4b57e1757d88f3cc8ac01fe5a12910387f
Chris Vergaray <ver08007@byui.edu>
2014-10-04 20:50:53 -0600

    updated form2

--- form2.php
+++ form2.php
@@ -7,10 +7,27 @@ session_start();
 
 <?php
 // Set session variables
-$_SESSION["favcolor"] = "green";
-$_SESSION["favanimal"] = "cat";
+$_SESSION["name"] = "green";
+$_SESSION["email"] = "cat";
 echo "Session variables are set.";
 ?>
+
+Name: <input type="text" name="name" value="<?php echo $_SESSION["name"];?>">
+
+E-mail: <input type="text" name="email" value="<?php echo $_SESSION["email"];?>">
+
+Website: <input type="text" name="website" value="<?php echo $_SESSION["website"];?>">
+
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $_SESSION["comment"];?></textarea>
+
+Gender:
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="female") echo "checked";?>
+value="female">Female
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="male") echo "checked";?>
+value="male">Male
+
 
 </body>
 </html>
