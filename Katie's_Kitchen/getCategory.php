ce523e7e53791b10eea23e2a349eed841c4df3f1
Chris Vergaray <ver08007@byui.edu>
2014-10-19 15:54:21 -0600

    added some temporary test code

--- Katie's_Kitchen/getCategory.php
+++ Katie's_Kitchen/getCategory.php
@@ -1,5 +1,6 @@
 
 <?php
+
 try {
    echo '<form action="KKitchen_results.php" method="POST" >';
    echo 'Product Type: <select name="category">';
@@ -17,6 +18,19 @@ try {
    }
    echo '</select>';
    echo '<input type="submit" value="Submit"></form>';
+
+
+   $query = "SELECT * FROM item WHERE item_type = (SELECT baked_good_id FROM baked_good Where item_name = 'Pies'";
+
+   $stmt = $db->prepare($query);
+//$stmt->bindValue(':book', $category, PDO::PARAM_STR);
+   $stmt->execute();
+   while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      var_dump($row);
+      echo '<H3>' . $row['item_name'] . '</h3> <br>';
+      echo 'item_description';
+      echo 'Price: $' . ($row['price'] / 100.0) . '<br> <hr>';
+   }
 } catch (PDOException $ex) {
    echo "Error connecting to DB. Details: $ex";
    die();
