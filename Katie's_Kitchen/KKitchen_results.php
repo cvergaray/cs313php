c5c3b65adaaf623278eca0bbd06bd498e1e6be42
Chris Vergaray <ver08007@byui.edu>
2014-10-19 18:21:01 -0600

    Ironing out price bugs

--- Katie's_Kitchen/KKitchen_results.php
+++ Katie's_Kitchen/KKitchen_results.php
@@ -24,8 +24,8 @@ $query = "SELECT * FROM item WHERE item_type = (SELECT baked_good_id FROM baked_
       echo '<H3>' . $row['item_name'] . '</h3> <br>';
       echo '<p>' . $row['item_description'] . '</p>';
       $cost = $row['item_price'];
-      $dollars = ( $cost / 100);
-      $pennies = ( $cost % 100);
+      $dollars = (int) ( $cost / 100);
+      $pennies = (int) ( $cost % 100);
 
       echo 'Price: $' . $dollars . '.' . $pennies . '<br> <hr>';
 }
