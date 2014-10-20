b5dedbe0e412a32158def8a10f2163b2ce239caa
Chris Vergaray <ver08007@byui.edu>
2014-10-19 18:18:12 -0600

    Trying to make the price work right.

--- Katie's_Kitchen/KKitchen_results.php
+++ Katie's_Kitchen/KKitchen_results.php
@@ -23,7 +23,7 @@ $query = "SELECT * FROM item WHERE item_type = (SELECT baked_good_id FROM baked_
       var_dump($row);
       echo '<H3>' . $row['item_name'] . '</h3> <br>';
       echo '<p>' . $row['item_description'] . '</p>';
-      $cost = $row['price'];
+      $cost = $row['item_price'];
       $dollars = ( $cost / 10);
       $pennies = ( $cost % 10);
       var_dump($cost);
