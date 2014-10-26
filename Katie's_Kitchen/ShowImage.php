d2769a5f5e3c333a8f85d424fb62ad0650416912
Chris Vergaray <ver08007@byui.edu>
2014-10-26 00:04:42 -0600

    Corrected the query

--- Katie's_Kitchen/ShowImage.php
+++ Katie's_Kitchen/ShowImage.php
@@ -4,7 +4,7 @@ include 'dbConnection.php';
 if(issset($_GET['id']))
 {
    $id = mysql_real_escape_string($_GET['id']);   
-   $newQuery = $db->query("SELECT * FROM image WHERE 'id' = '$id'");
+   $newQuery = $db->query("SELECT * FROM image WHERE 'image_id' = '$id'");
    $newQuery->execute();
 
    while ($newRow = $newQuery->fetch(PDO::FETCH_ASSOC)) {
