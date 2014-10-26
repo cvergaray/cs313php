182027a36c5f5eed5955df918d2418e5de920354
Chris Vergaray <ver08007@byui.edu>
2014-10-26 00:18:12 -0600

    Added image section to results

--- Katie's_Kitchen/KKitchen_results.php
+++ Katie's_Kitchen/KKitchen_results.php
@@ -10,24 +10,25 @@ echo '<h1>' . $category . '</h1>';
 
 $query = "SELECT * FROM item WHERE item_type = (SELECT baked_good_id FROM baked_good Where item_name = '" . $category . "'";
 
-   $query = "SELECT baked_good_id FROM baked_good WHERE item_name = '" . $category . "'";
-   $stmt = $db->prepare($query);
-   $stmt->execute();
-   $itemNum = $stmt->fetch(PDO::FETCH_ASSOC)["baked_good_id"];
-   
-   $query = "SELECT * FROM item WHERE item_type = " . $itemNum;//(SELECT baked_good_id FROM baked_good WHERE item_name = 'Pies'";
-   $stmt = $db->prepare($query);
-   $stmt->execute();
-   
-   while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      //var_dump($row);
-      echo '<H3>' . $row['item_name'] . '</h3> <br>';
-      echo '<p>' . $row['item_description'] . '</p>';
-      $cost = $row['item_price'];
-      $dollars = (int) ($cost / 100);
-      $pennies = sprintf("%02s", ($cost % 100));
+$query = "SELECT baked_good_id FROM baked_good WHERE item_name = '" . $category . "'";
+$stmt = $db->prepare($query);
+$stmt->execute();
+$itemNum = $stmt->fetch(PDO::FETCH_ASSOC)["baked_good_id"];
 
-      echo 'Price: $' . $dollars . '.' . round($pennies,2) . '<br> <hr>';
+$query = "SELECT * FROM item WHERE item_type = " . $itemNum; //(SELECT baked_good_id FROM baked_good WHERE item_name = 'Pies'";
+$stmt = $db->prepare($query);
+$stmt->execute();
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+   //var_dump($row);
+   echo '<H3>' . $row['item_name'] . '</h3>';
+   echo '<img src="ShowImage.php?id="' . $itemNum . '"> <br>';
+   echo '<p>' . $row['item_description'] . '</p>';
+   $cost = $row['item_price'];
+   $dollars = (int) ($cost / 100);
+   $pennies = sprintf("%02s", ($cost % 100));
+
+   echo 'Price: $' . $dollars . '.' . round($pennies, 2) . '<br> <hr>';
 }
 ?>
 
