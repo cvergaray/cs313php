73dda396d1ab88c0b25a6f9f6c5feb452e34536f
Chris Vergaray <ver08007@byui.edu>
2014-10-26 00:35:04 -0600

    added debug code

--- Katie's_Kitchen/ShowImage.php
+++ Katie's_Kitchen/ShowImage.php
@@ -3,12 +3,14 @@ include 'dbConnection.php';
 
 if(issset($_GET['id']))
 {
-   $id = mysql_real_escape_string($_GET['id']);   
+   $id = mysql_real_escape_string($_GET['id']);
+   var_dump($id);
    $newQuery = $db->query("SELECT * FROM image WHERE image_id = $id");
    $newQuery->execute();
 
    while ($newRow = $newQuery->fetch(PDO::FETCH_ASSOC)) {
       $imageData = $row["image_data"];
+      var_dump($imageData);
    }
    header("content-type: image/jpeg");
    echo $imageData;
