1d0b7d8ab7d8cb032e162fb5431318afb1c001c4
Chris Vergaray <ver08007@byui.edu>
2014-10-27 12:22:10 -0600

    Upload now uploads all items from the form

--- Katie's_Kitchen/add_product.php
+++ Katie's_Kitchen/add_product.php
@@ -14,22 +14,20 @@ $topics = $db->query("SELECT * FROM baked_good");
 //Make form
 ?>
 <form action="upload.php" method="POST" enctype="multipart/form-data">
-   Item Name: <input name="book"><br><br>
-   Price: <input name="verse"><br><br>
-   Item Description: <textarea name="content" rows="5" cols="40"></textarea><br><br>
-   Topic(s):<br>
+   Item Name: <input name="item_name"><br><br>
+   Price: <input name="item_price"><br><br>
+   Item Description: <textarea name="item_description" rows="5" cols="40"></textarea><br><br>
    <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
    <input name="userfile" type="file" id="userfile"> 
-   
-
+   <br>Category:<br>
    <?php
 //Make checkboxes
    while ($row = $topics->fetch(PDO::FETCH_ASSOC)) {
-      echo '<input type="checkbox" name="topics[]" value=' . $row['baked_good_id'] . '>' . $row['item_name'] . '<br>';
+      echo '<input type="radio" name="category" value=' . $row['baked_good_id'] . '>' . $row['item_name'] . '<br>';
    }
 
 //Make other checkbox
-   echo '<input type="checkbox" name="topics[]" value="NULL">Other <input name="New"><br><br>';
+   echo '<input type="radio" name="category" value="NULL">New <input name="New"><br><br>';
 
    echo '<br><input name="upload" type="submit" class="box" id="upload" value=" Upload ">
 	</form>';
--- Katie's_Kitchen/upload.php
+++ Katie's_Kitchen/upload.php
@@ -1,31 +1,47 @@
 <?php
-if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
-{
-$fileName = $_FILES['userfile']['name'];
-$tmpName  = $_FILES['userfile']['tmp_name'];
-$fileSize = $_FILES['userfile']['size'];
-$fileType = $_FILES['userfile']['type'];
 
-$fp      = fopen($tmpName, 'r');
-$content = fread($fp, filesize($tmpName));
-$content = addslashes($content);
-fclose($fp);
+if (isset($_POST['upload']) && $_FILES['userfile']['size'] > 0 && isset($_POST['item_name']) && isset($_POST['item_price']) && isset($_POST['item_description']) && isset($_POST['category'])) {
+   $fileName = $_FILES['userfile']['name'];
+   $tmpName = $_FILES['userfile']['tmp_name'];
+   $fileSize = $_FILES['userfile']['size'];
+   $fileType = $_FILES['userfile']['type'];
 
-if(!get_magic_quotes_gpc())
-{
-    $fileName = addslashes($fileName);
-}
+   $fp = fopen($tmpName, 'r');
+   $content = fread($fp, filesize($tmpName));
+   $content = addslashes($content);
+   fclose($fp);
+
+   if (!get_magic_quotes_gpc()) {
+      $fileName = addslashes($fileName);
+   }
 
 //Connect to the database
-include 'dbConnection.php';
-$db = loadDB();
+   include 'dbConnection.php';
+   $db = loadDB();
 
 
-$query = "INSERT INTO image (image_id, image_name, image_data ) ".
-"VALUES (NULL, '$fileName', '$content')";
-$query = $db->prepare($query);
-$query->execute();
+   $query = "INSERT INTO image (image_id, image_name, image_data, creation_date ) " .
+           "VALUES (NULL, '$fileName', '$content', UTC_DATE())";
+   $query = $db->prepare($query);
+   $query->execute();
 
-echo "<br>File $fileName uploaded<br>";
-} 
+   $modifiedPrice = $_POST['item_price'] * 100;
+
+   $query = "INSERT INTO item (item_id, item_type, item_name, item_description," .
+           " item_price, item_picture, creation_date) " .
+           "VALUES(NULL, " .
+           $_POST['category'] .
+           ", " . $_POST['item_name'] .
+           ", " . $_POST['item_description'] .
+           ", " . $modifiedPrice .
+           ", (SELECT image_id FROM image WHERE image_name = '$fileName')" .
+           ", UTC_DATE());";
+
+   $query = $db->prepare($query);
+   $query->execute();
+
+   echo "<br>File $fileName uploaded<br>";
+} else {
+   echo "<br>Please fill out entire form and include image<br>";
+}
 ?>
