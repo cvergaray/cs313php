fdf1aababc242cb77cef4736f1beeaa0305795b3
Chris Vergaray <ver08007@byui.edu>
2014-10-27 00:50:57 -0600

    Testing the test

--- Katie's_Kitchen/ShowImage.php
+++ Katie's_Kitchen/ShowImage.php
@@ -1,23 +1,18 @@
 <?php
-echo 'including file';
 include 'dbConnection.php';
-echo ' included';
 $db = loadDB();
-echo ' db loaded';
 
 if ($_GET['id'] === NULL) {
    echo 'error!';    
 } else {
-   echo ' is set';
    $id = $_GET['id'];
-   echo (' id= ' . $id);
-   var_dump($id);
    $newQuery = ("SELECT * FROM image WHERE image_id = " . $id);
    var_dump($newQuery);
    $newStmt = $db->prepare($newQuery);
    $newStmt->execute();
 
    while ($newRow = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      echo 'found a row!';
       $imageData = $newRow["image_data"];
       var_dump($imageData);
    }
