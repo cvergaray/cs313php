4a0409c689ee9f1c7d965f2192bd0b52371f7e42
Chris Vergaray <ver08007@byui.edu>
2014-10-27 01:45:06 -0600

    Added some adding functionality

--- Katie's_Kitchen/add_product.php
+++ Katie's_Kitchen/add_product.php
@@ -9,20 +9,19 @@ echo '<h1>New Scripture Entry</h1>';
 	$db = loadDB();
 	
 	//Get topics
-	$topics = $db->query("SELECT * FROM item");
+	$topics = $db->query("SELECT * FROM baked_good");
 
 	//Make form
 	echo '<form action="" method="POST" >
-		Book: <input name="book"><br><br>
-		Chapter: <input name="chapter"><br><br>
-		Verse: <input name="verse"><br><br>
-		Content: <textarea name="content" rows="5" cols="40"></textarea><br><br>';
+		Item Name: <input name="book"><br><br>
+		Price: <input name="verse"><br><br>
+		Item Description: <textarea name="content" rows="5" cols="40"></textarea><br><br>';
 	echo 'Topic(s):<br>';
 	
 	//Make checkboxes
 	while($row = $topics->fetch(PDO::FETCH_ASSOC)) 
 		{
-			echo '<input type="checkbox" name="topics[]" value=' . $row['name'] . '>' . $row['name'] . '<br>';
+			echo '<input type="checkbox" name="topics[]" value=' . $row['baked_good_id'] . '>' . $row['item_name'] . '<br>';
 		}
 	
 	//Make other checkbox
