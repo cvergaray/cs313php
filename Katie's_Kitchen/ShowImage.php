73767db274666b6c55eea18c58a532c9bae9c731
Chris Vergaray <ver08007@byui.edu>
2014-10-26 18:17:25 -0600

    added debug code

--- Katie's_Kitchen/ShowImage.php
+++ Katie's_Kitchen/ShowImage.php
@@ -1,24 +1,20 @@
 <?php
 
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = 'php';
-$dbPassword = 'php-pass-150864067';
-var_dump($dbHost);
-var_dump($dbPassword);
-var_dump($dbPort);
-var_dump($dbUser);
+include dbConnection.php;
 
-mysql_connect($dbHost . ':' . $dbPort, $dbUser, $dbPassword);
-mysql_select_db('katie_db');
+loadDB();
+
+var_dump($_GET);
 
 if (issset($_GET['id'])) {
    $id = mysql_real_escape_string($_GET['id']);
    var_dump($id);
-   $newQuery = mysql_query("SELECT * FROM image WHERE image_id = " . $id);
+   $newQuery = "SELECT * FROM image WHERE image_id = " . $id;
+   $stmt = $db->prepare($newQuery);
+$stmt->execute();
 
    while ($newRow = mysql_fetch_assoc($newQuery)) {
-      $imageData = $row["image_data"];
+      $imageData = $newRow["image_data"];
       var_dump($imageData);
    }
    header("content-type: image/jpeg");
