ca7c8a6f58cf52c230df647594c360aee450a83a
Chris Vergaray <ver08007@byui.edu>
2014-10-26 18:25:43 -0600

    added debug code

--- Katie's_Kitchen/ShowImage.php
+++ Katie's_Kitchen/ShowImage.php
@@ -1,19 +1,19 @@
 <?php
 
-include dbConnection.php;
-
-loadDB();
-
 var_dump($_GET);
+
+include dbConnection . php;
+
+$db = loadDB();
 
 if (issset($_GET['id'])) {
    $id = mysql_real_escape_string($_GET['id']);
    var_dump($id);
    $newQuery = "SELECT * FROM image WHERE image_id = " . $id;
-   $stmt = $db->prepare($newQuery);
-$stmt->execute();
+   $newStmt = $db->prepare($newQuery);
+   $newStmt->execute();
 
-   while ($newRow = mysql_fetch_assoc($newQuery)) {
+   while ($newRow = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $imageData = $newRow["image_data"];
       var_dump($imageData);
    }
