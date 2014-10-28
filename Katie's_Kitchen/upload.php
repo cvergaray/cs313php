50ba7d96e26d783e87883b8fb04351ef5dd097bf
Chris Vergaray <ver08007@byui.edu>
2014-10-27 21:06:56 -0600

    Query should be correctly formatted and debug code has been removed.

--- Katie's_Kitchen/upload.php
+++ Katie's_Kitchen/upload.php
@@ -27,23 +27,18 @@ if (isset($_POST['upload']) && $_FILES['userfile']['size'] > 0 && isset($_POST['
 
    $modifiedPrice = $_POST['item_price'] * 100;
 
-   var_dump($_POST);
-   var_dump($modifiedPrice);
-   var_dump($fileName);
    $query = "INSERT INTO item (item_id, item_type, item_name, item_description," .
            " item_price, item_picture, creation_date) " .
            "VALUES(NULL, '" .
            $_POST['category'] .
            "', '" . $_POST['item_name'] .
-           "', " . $_POST['item_description'] .
-           "', " . $modifiedPrice .
+           "', '" . $_POST['item_description'] .
+           "', '" . $modifiedPrice .
            "', (SELECT image_id FROM image WHERE image_name = '$fileName')" .
            ", UTC_DATE());";
 
-   echo '<p>' . $query . '</p>';
    $query = $db->prepare($query);
    $query->execute();
-   var_dump($query->errorInfo());
 
    echo "<br>File $fileName uploaded<br>";
 } else {
