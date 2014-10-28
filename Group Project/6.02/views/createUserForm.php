ee5be3b8843bdef3ee43c308259e1476d9c9d0f6
Chris Vergaray <ver08007@byui.edu>
2014-10-28 11:27:23 -0600

    Trying to maintain consistency

--- Group Project/6.02/views/createUserForm.php
+++ Group Project/6.02/views/createUserForm.php
@@ -5,27 +5,27 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <form method="POST">
-		<table style="width:100%">
-			<tr>
-			<td>Username: <input name="name"><br></td>
-			 </tr>
-			  <tr>
-				<td>Password: <input type="password" name="pass"><br></td>
-			  </tr>
-			  <tr>
-				<td>Re-enter Password: <input type="password" name="check"><br></td>
-			  </tr>
-			  <tr>
-				<td><input type="submit" value="Create"></td>
-			  </tr>
-			</table>
-            <input type="hidden" name="action" value="CreateUser">
-        </form>
-    </body>
+   <head>
+      <meta charset="UTF-8">
+      <title></title>
+   </head>
+   <body>
+      <form method="POST">
+         <table style="width:100%">
+            <tr>
+               <td>Username: <input name="name"><br></td>
+            </tr>
+            <tr>
+               <td>Password: <input type="password" name="pass"><br></td>
+            </tr>
+            <tr>
+               <td>Re-enter Password: <input type="password" name="check"><br></td>
+            </tr>
+            <tr>
+               <td><input type="hidden" name="action" value="CreateUser">
+                  <input type="submit" value="Create"></td>
+            </tr>
+         </table>
+      </form>
+   </body>
 </html>
