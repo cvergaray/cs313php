e4d2c61f996bceb3d143e1bb124c1037b97a29bb
Chris Vergaray <ver08007@byui.edu>
2014-10-29 14:45:53 -0600

    Adding Style

---
+++ Katie's_Kitchen/KKHead.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Katie's Kitchen</title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="KKitchen.css" type="text/css" media="screen">
+      <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+      <link href="http://fonts.googleapis.com/css?family=PT+Serif+Caption:400,400italic" rel="stylesheet" type="text/css">
+   </head>
--- Katie's_Kitchen/KKitchen.php
+++ Katie's_Kitchen/KKitchen.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-   <head>
-      <meta charset="UTF-8">
-      <title>Katie's Kitchen</title>
-   </head>
+<?PHP
+include 'KKhead.php';
+?>
+
    <body>
       <h1>Katie's Kitchen</h1>
       <?php
