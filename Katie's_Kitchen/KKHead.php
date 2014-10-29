f3a491aa44fc60db6ae3da56084e71ba8325dcf9
Chris Vergaray <ver08007@byui.edu>
2014-10-29 14:54:24 -0600

    Revised the header so it actually shows up

--- Katie's_Kitchen/KKHead.php
+++ Katie's_Kitchen/KKHead.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <title>Katie's Kitchen</title>
-      <meta charset="utf-8">
-      <link rel="stylesheet" href="KKitchen.css" type="text/css" media="screen">
-      <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-      <link href="http://fonts.googleapis.com/css?family=PT+Serif+Caption:400,400italic" rel="stylesheet" type="text/css">
-   </head>
+<?php 
+echo "<!DOCTYPE html>\r\n";
+echo "<html lang=\"en\">\r\n";
+echo "   <head>'\r\n";
+echo "      <title>Katie's Kitchen</title>\r\n";
+echo "      <meta charset=\"utf-8\">\r\n";
+echo "      <link rel=\"stylesheet\" href=\"KKitchen.css\" type=\"text/css\" media=\"screen\">\r\n";
+echo "      <link rel=\"icon\" href=\"images/favicon.ico\" type=\"image/x-icon\">\r\n";
+echo "      <link href=\"http://fonts.googleapis.com/css?family=PT+Serif+Caption:400,400italic\" rel=\"stylesheet\" type=\"text/css\">\r\n";
+echo "   </head>\r\n";
