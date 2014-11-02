f665a65bba0562f7c40a43e8cbaf309f30330110
Chris Vergaray <ver08007@byui.edu>
2014-11-01 23:50:24 -0600

    Updating CSS styles

--- Katie's_Kitchen/KKitchen.php
+++ Katie's_Kitchen/KKitchen.php
@@ -9,13 +9,15 @@
       include 'getCategory2.php';
       ?>
       <br/>
-      <?php
-      if (time() % 2 == 0) {
-         echo '<img class="katieProfile" width=400 src="ShowImage.php?id=19"/>';
-      } else {
-         echo '<img class="katieProfile" width=400 src="ShowImage.php?id=20"/>';
-      }
-      ?>
+      <div class="katieProfile">
+         <?php
+         if (time() % 2 == 0) {
+            echo '<img width=400 src="ShowImage.php?id=19"/>';
+         } else {
+            echo '<img width=400 src="ShowImage.php?id=20"/>';
+         }
+         ?>
+      </div>
       <br/>
    </body>
 </html>
