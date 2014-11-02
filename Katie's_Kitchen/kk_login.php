25db52031b10e49db134f82dbb3a7f9dde8bca5e
Chris Vergaray <ver08007@byui.edu>
2014-11-02 01:06:14 -0700

    Updating login functionality

--- Katie's_Kitchen/kk_login.php
+++ Katie's_Kitchen/kk_login.php
@@ -1,5 +1,5 @@
 <?php
-
+phpAlert("Just loading the page");
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
