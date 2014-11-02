4f6b4e185324509b964da924175f77f27ebf4a7c
Chris Vergaray <ver08007@byui.edu>
2014-11-01 18:10:15 -0600

    Updating CSS styles

--- Katie's_Kitchen/KKitchen.php
+++ Katie's_Kitchen/KKitchen.php
@@ -1,12 +1,21 @@
 <html>
-<?PHP
-include 'KKHead.php';
-?>
+   <?PHP
+   include 'KKHead.php';
+   ?>
    <body>
-      <h1>Katie's Kitchen</h1> <br/> <br/>
+      <br><br>
+      <h1>Katie's Kitchen</h1> <br/>
       <?php
       include 'getCategory2.php';
       ?>
-      <br/><br/>
+      <br/>
+      <?php
+      if (time() % 2 == 0) {
+         echo '<img width=400 src="ShowImage.php?id=19">';
+      } else {
+         echo '<img width=400 src="ShowImage.php?id=20">';
+      }
+      ?>
+      <br/>
    </body>
 </html>
