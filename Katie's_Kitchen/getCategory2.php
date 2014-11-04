bf8927f7cf2f0a861ac561c48ac4d47753a92723
Chris Vergaray <ver08007@byui.edu>
2014-11-03 22:15:04 -0700

    added debug code

--- Katie's_Kitchen/getCategory2.php
+++ Katie's_Kitchen/getCategory2.php
@@ -1,22 +1,25 @@
 <?php
-
+$debug = TRUE;
 try {
 //   echo '<form action="KKitchen_results.php" method="POST" >';
    echo '<header>';
-
+   phpAlert("Starting session");
+   
    // Start the session           
    session_start();
    include 'dbConnection.php';
    $db = loadDB();
    $stmt = $db->query("SELECT * FROM baked_good");
-   $stmt->execute();
+   $success = $stmt->execute();
 
+   phpAlert("Success: $success");
+   
    while ($headrow = $stmt->fetch(PDO::FETCH_ASSOC)) {
       echo '<a href="KKitchen_results.php?category=' . $headrow['item_name'] . '">' . $headrow['item_name'] .'</a> &nbsp;';
    }
    echo '<br><a href="cart.php">View Cart </a>&nbsp;&nbsp;';
    
-   if($_SESSION['authenticated']){
+   if(isset($_SESSION['authenticated']) && $_SESSION['authenticated']){
       echo "Welcome {$_SESSION['currentUser']}!";      
       echo '&nbsp;&nbsp;<a href="kk_login.php?action=logout">Sign Out</a>';
    }   
@@ -27,5 +30,11 @@ try {
   echo '</header>';
 } catch (PDOException $ex) {
    echo "Error connecting to DB. Details: $ex";
-   //die();
+   die();
+}
+
+function phpAlert($msg) {
+   if ($debug) {
+      echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+   }
 }
